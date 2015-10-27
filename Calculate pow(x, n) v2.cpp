aabed8373fe9d8fd403e02d6eacfa594180fd7b0
Rabiul Awal <awalrabiul6@gmail.com>
2015-10-27 15:59:03 +0600

    Create Calculate pow(x, n) v2.cpp

---
+++ Calculate pow(x, n) v2.cpp
@@ -0,0 +1,24 @@
+#include<bits//stdc++.h>
+using namespace std;
+
+int power(int x, unsigned int y)
+{
+    int temp;
+    if( y == 0)
+        return 1;
+    temp = power(x, y/2);
+    if (y%2 == 0)
+        return temp*temp;
+    else
+        return x*temp*temp;
+
+}
+
+int main()
+{
+    int x;
+    unsigned int y;
+    cin>>x>>y;
+    printf("%d", power(x, y));
+    return 0;
+}

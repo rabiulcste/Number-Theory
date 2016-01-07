de5adf725931e8776139217c25b74ff3d863feb8
Rabiul Awal <awalrabiul6@gmail.com>
2016-01-08 04:19:44 +0600

    Combination

---
+++ Combination - NCR .cpp
@@ -0,0 +1,58 @@
+#include<cstdio>
+#include<sstream>
+#include<cstdlib>
+#include<cctype>
+#include<cmath>
+#include<algorithm>
+#include<set>
+#include<queue>
+#include<stack>
+#include<list>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cstring>
+#include<map>
+#include<cassert>
+#include<climits>
+using namespace std;
+
+#define REP(i,n) for(int i=0, _e(n); i<_e; i++)
+#define FOR(i,a,b) for(int i(a), _e(b); i<=_e; i++)
+#define FORD(i,a,b) for(int i(a), _e(b); i>=_e; i--)
+#define FORIT(i, m) for (__typeof((m).begin()) i=(m).begin(); i!=(m).end(); ++i)
+#define SET(t,v) memset((t), (v), sizeof(t))
+#define ALL(x) x.begin(), x.end()
+#define UNIQUE(c) (c).resize( unique( ALL(c) ) - (c).begin() )
+
+#define sz size()
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+
+typedef long long LL;
+typedef long double LD;
+typedef pair<int,int> pii;
+
+#define D(x) if(1) cout << __LINE__ <<" "<< #x " = " << (x) << endl;
+#define D2(x,y) if(1) cout << __LINE__ <<" "<< #x " = " << (x) \
+    <<", " << #y " = " << (y) << endl;
+
+LL ncr[100][100];
+
+int main() {
+
+    // ncr calculating
+    ncr[0][0] = 1;
+    FOR(i,1,80) {
+        ncr[i][0] = 1;
+        FOR(j,1,80) ncr[i][j] = ncr[i-1][j] + ncr[i-1][j-1];
+    }
+
+
+    int n, m;
+    cin >> n >> m;
+    LL ans = ncr[n][m];
+    cout << ans << endl;
+    return 0;
+}

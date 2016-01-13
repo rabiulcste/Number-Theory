e1ad7b76fdb5a97f6e6e716ef44106965725f85c
Rabiul Awal <awalrabiul6@gmail.com>
2016-01-14 05:56:02 +0600

    Big Number Comparing

---
+++ Comparing Two Big Number.cpp
@@ -0,0 +1,39 @@
+#include <bits//stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    string a, b;
+    cin>>a>>b;
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+    while(a.back() == '0') a.pop_back();
+    while(b.back() == '0') b.pop_back();
+
+    if(a.size() > b.size()) {
+            cout<<">"<<endl;
+            return 0;
+    }
+    else if(a.size() < b.size()) {
+        cout<<"<"<<endl;
+        return 0;
+    }
+    else {
+        for(int i = (int)a.size()-1; i>=0; i--){
+            if(a[i]>b[i]) {
+                cout<<">"<<endl;
+                return 0;
+            }
+            else if(a[i]<b[i]){
+                cout<<"<"<<endl;
+                return 0;
+            }
+        }
+        cout<<"="<<endl;
+    }
+
+    return 0;
+}
